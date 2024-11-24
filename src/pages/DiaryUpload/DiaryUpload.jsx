b9e822c123a78986e6f8775e209741557fc59c13
Beta_Rigel <qocksgur1234@naver.com>
2024-11-24 16:56:53 +0900

    first comit

---
+++ src/pages/DiaryUpload/DiaryUpload.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import styles from './DiaryUpload.module.css';
+
+
+const DiaryUpload = () => {
+    return ( 
+    <div className={styles.DiaryUpload}>
+        <div className={styles.MainContainer}>
+            dfsdfsdfsdf
+        </div>
+    </div>
+    );
+};
+
+export default DiaryUpload;
