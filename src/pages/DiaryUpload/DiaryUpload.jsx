5887240e62f834d49664e9288410bbca965f69c7
Beta_Rigel <qocksgur1234@naver.com>
2024-11-24 20:00:19 +0900

    forth commit

--- src/pages/DiaryUpload/DiaryUpload.jsx
+++ src/pages/DiaryUpload/DiaryUpload.jsx
@@ -36,7 +36,18 @@ const DiaryUpload = () => {
                 
                     </div>
                     <div className={styles.SubImg}>
-
+                        <div className={styles.SubImg1}>
+                        
+                        </div>
+                        <div className={styles.SubImg2}>
+                        
+                        </div>
+                        <div className={styles.SubImg3}>
+                        
+                        </div>
+                        <div className={styles.SubImg4}>
+                        
+                        </div>
                     </div>
                 </div>
 
