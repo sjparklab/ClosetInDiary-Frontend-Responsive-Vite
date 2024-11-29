a750151bfc622d3fcd4fe42b6ab4efa51307e542
sjtemp62 <sjtemp62@gmail.com>
2024-11-29 22:46:31 +0900

    push System test

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,118 +1,118 @@
 import React from "react";
+import styles from "./Footer.module.css";
 import group27 from "./email-logo.svg";
 import image from "./footer-line-16.svg";
 import line15 from "./footer-line-15.svg";
 import line16 from "./footer-line-16.svg";
-import "./Footer.css";
 import vector from "./footer-logo.svg";
 
 const Footer = () => {
     return (
-        <div className="category-footer">
-            <div className="frame">
-                <div className="group">
-                    <img className="vector" alt="Vector" src={vector} />
+        <div className={styles.categoryFooter}>
+            <div className={styles.frame}>
+                <div className={styles.group}>
+                    <img className={styles.vector} alt="Vector" src={vector} />
 
-                    <div className="div">
-                        <div className="text-wrapper">contact@stand.agency</div>
+                    <div className={styles.div}>
+                        <div className={styles.textWrapper}>contact@stand.agency</div>
 
-                        <img className="line" alt="Line" src={line15} />
+                        <img className={styles.line} alt="Line" src={line15} />
                     </div>
                 </div>
 
-                <div className="group-2">
-                    <div className="group-3">
-                        <div className="flexcontainer">
-                            <p className="text">
-                                <span className="span">
+                <div className={styles.group2}>
+                    <div className={styles.group3}>
+                        <div className={styles.flexContainer}>
+                            <p className={styles.text}>
+                                <span className={styles.span}>
                                     Facebook
                                     <br />
                                 </span>
                             </p>
 
-                            <p className="text">
-                                <span className="span">
+                            <p className={styles.text}>
+                                <span className={styles.span}>
                                     Linkedin
                                     <br />
                                 </span>
                             </p>
 
-                            <p className="text">
-                                <span className="span">
+                            <p className={styles.text}>
+                                <span className={styles.span}>
                                     Instagram
                                     <br />
                                 </span>
                             </p>
 
-                            <p className="text">
-                                <span className="span">Twitter</span>
+                            <p className={styles.text}>
+                                <span className={styles.span}>Twitter</span>
                             </p>
                         </div>
 
-                        <div className="text-wrapper-2">Socials</div>
+                        <div className={styles.textWrapper2}>Socials</div>
 
-                        <div className="group-4">
-                            <div className="text-wrapper">(123) 456-7890</div>
+                        <div className={styles.group4}>
+                            <div className={styles.textWrapper}>(123) 456-7890</div>
 
-                            <img className="img" alt="Line" src={image} />
+                            <img className={styles.img} alt="Line" src={image} />
                         </div>
                     </div>
 
-                    <div className="sitemap">
-                        <div className="flexcontainer-2">
-                            <p className="text">
-                                <span className="span">
+                    <div className={styles.sitemap}>
+                        <div className={styles.flexContainer2}>
+                            <p className={styles.text}>
+                                <span className={styles.span}>
                                     Home
                                     <br />
                                 </span>
                             </p>
 
-                            <p className="text">
-                                <span className="span">
+                            <p className={styles.text}>
+                                <span className={styles.span}>
                                     Abouts
                                     <br />
                                 </span>
                             </p>
 
-                            <p className="text">
-                                <span className="span">
+                            <p className={styles.text}>
+                                <span className={styles.span}>
                                     Growers
                                     <br />
                                 </span>
                             </p>
 
-                            <p className="text">
-                                <span className="span">
+                            <p className={styles.text}>
+                                <span className={styles.span}>
                                     Merchants
                                     <br />
                                 </span>
                             </p>
 
-                            <p className="text">
-                                <span className="span">Contact</span>
+                            <p className={styles.text}>
+                                <span className={styles.span}>Contact</span>
                             </p>
                         </div>
 
-                        <div className="text-wrapper-3">Sitemap</div>
+                        <div className={styles.textWrapper3}>Sitemap</div>
                     </div>
                 </div>
 
-                <div className="group-5">
-                    <p className="p">
+                <div className={styles.group5}>
+                    <p className={styles.p}>
                         52-57, Yangjeong-ro, Busanjin-gu, Busan, Republic of Korea
                     </p>
 
-                    <div className="text-wrapper-3">Head Office</div>
+                    <div className={styles.textWrapper3}>Head Office</div>
 
-                    <div className="text-wrapper-4">News letter</div>
+                    <div className={styles.textWrapper4}>News letter</div>
 
-                    <img className="line-2" alt="Line" src={line16} />
+                    <img className={styles.line2} alt="Line" src={line16} />
 
-                    <div className="text-wrapper-5">Enter your email address</div>
+                    <div className={styles.textWrapper5}>Enter your email address</div>
 
-                    <img className="group-6" alt="Group" src={group27} />
+                    <img className={styles.group6} alt="Group" src={group27} />
 
-                    <p className="element-ST-all-rights">
+                    <p className={styles.elementSTAllRights}>
                         © 2024 ST&amp; All rights reserved.
                     </p>
                 </div>
@@ -120,6 +120,5 @@ const Footer = () => {
         </div>
     );
 };
-
 
 export default Footer;
--- src/pages/ClosetAddPage/ClosetAddPage.jsx
+++ src/pages/ClosetAddPage/ClosetAddPage.jsx
@@ -1,13 +1,57 @@
 import React from 'react';
-import styles from './ClosetAddPage.module.css';
-import Header from '../../components/NormalHeader';
-
+import styles from './ClosetAddPage.module.css'; // Import the CSS module
 
 const ClosetAddPage = () => {
   return (
-    <div className={styles.TEST}>
-      <Header />
+<div style={{width: 965, height: 1022, position: 'relative'}}>
+  <div style={{width: 693, height: 65, left: 136, top: 0, position: 'absolute', borderBottom: '5px #111111 solid'}}>
+    <div style={{left: 0, top: 15, position: 'absolute', textAlign: 'center', color: '#111111', fontSize: 36, fontFamily: 'Pretendard', fontWeight: '600', lineHeight: 35, wordWrap: 'break-word'}}>이 옷이 담아낼 당신의 이야기는 어떤 모습일까요?</div>
+  </div>
+  <div style={{paddingLeft: 188, paddingRight: 188, paddingTop: 303, paddingBottom: 303, left: 0, top: 244, position: 'absolute', background: 'white', border: '2px #111111 solid', justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
+    <div style={{width: 100, height: 100, padding: 23, borderRadius: 50, border: '2px #111111 solid', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'flex'}}>
+      <div style={{width: 54, height: 54, border: '2px #111111 solid'}}></div>
     </div>
+  </div>
+  <div style={{left: 566, top: 502, position: 'absolute', textAlign: 'center', color: '#111111', fontSize: 24, fontFamily: 'Pretendard', fontWeight: '600', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}>어떤 옷인가요?</div>
+  <div style={{width: 88, height: 50, left: 876, top: 972, position: 'absolute', background: '#444444', boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.25)', borderRadius: 10}}>
+    <div style={{width: 52, height: 30, left: 18, top: 10, position: 'absolute'}}>
+      <div style={{width: 52, height: 30, left: 0, top: 0, position: 'absolute', justifyContent: 'flex-start', alignItems: 'center', gap: 25, display: 'inline-flex'}}>
+        <div style={{color: 'white', fontSize: 20, fontFamily: 'Pretendard', fontWeight: '400', wordWrap: 'break-word'}}>업로드</div>
+      </div>
+    </div>
+  </div>
+  <div style={{width: 397, height: 76, left: 567, top: 335, position: 'absolute'}}>
+    <div style={{width: 397, height: 36, left: 0, top: 0, position: 'absolute', color: '#111111', fontSize: 24, fontFamily: 'Pretendard', fontWeight: '600', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}>이 옷을 사게 된 계기는 무엇인가요?</div>
+    <div style={{left: 0, top: 66, position: 'absolute', textAlign: 'center', color: 'rgba(102, 102, 102, 0.35)', fontSize: 20, fontFamily: 'Pretendard', fontWeight: '300', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}> 계기를 입력해주세요</div>
+  </div>
+  <div style={{width: 338, height: 224, left: 566, top: 577, position: 'absolute'}}>
+    <div style={{left: 1, top: 214, position: 'absolute', textAlign: 'center', color: '#111111', fontSize: 24, fontFamily: 'Pretendard', fontWeight: '500', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}>사이즈</div>
+    <div style={{left: 0, top: 76, position: 'absolute', textAlign: 'center', color: '#111111', fontSize: 24, fontFamily: 'Pretendard', fontWeight: '500', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}>구매일</div>
+    <div style={{left: 0, top: 0, position: 'absolute', textAlign: 'center', color: '#111111', fontSize: 24, fontFamily: 'Pretendard', fontWeight: '500', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}>카테고리</div>
+    <div style={{width: 338, height: 10, left: 0, top: 145, position: 'absolute'}}>
+      <div style={{left: 0, top: 0, position: 'absolute', textAlign: 'center', color: '#111111', fontSize: 24, fontFamily: 'Pretendard', fontWeight: '500', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}>브랜드</div>
+      <div style={{left: 160, top: 0, position: 'absolute', textAlign: 'center', color: 'rgba(102, 102, 102, 0.35)', fontSize: 20, fontFamily: 'Pretendard', fontWeight: '300', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}>브랜드를 입력해주세요</div>
+    </div>
+    <div style={{left: 160, top: 214, position: 'absolute', textAlign: 'center', color: 'rgba(102, 102, 102, 0.35)', fontSize: 20, fontFamily: 'Pretendard', fontWeight: '300', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}>사이즈를 입력해주세요</div>
+  </div>
+  <div style={{width: 365, height: 0, left: 570.50, top: 425.25, position: 'absolute', border: '1px rgba(102, 102, 102, 0.35) solid'}}></div>
+  <div style={{width: 238.51, height: 245.75, left: 726, top: 570, position: 'absolute'}}>
+    <div style={{width: 226, height: 24, left: 0, top: 0, position: 'absolute'}}>
+      <div style={{left: 0, top: 7, position: 'absolute', textAlign: 'center', color: 'rgba(102, 102, 102, 0.35)', fontSize: 20, fontFamily: 'Pretendard', fontWeight: '300', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}>카테고리를 선택해주세요</div>
+      <div style={{width: 24, height: 24, left: 202, top: 0, position: 'absolute'}}>
+        <div style={{width: 24, height: 24, left: 0, top: 0, position: 'absolute', background: '#D9D9D9'}} />
+        <div style={{width: 12, height: 7.40, left: 6, top: 8, position: 'absolute', background: 'rgba(102, 102, 102, 0.35)'}}></div>
+      </div>
+    </div>
+    <div style={{width: 238.01, height: 0, left: 0.49, top: 31.99, position: 'absolute', border: '1px rgba(102, 102, 102, 0.35) solid'}}></div>
+    <div style={{width: 238.01, height: 0, left: 0.50, top: 175.75, position: 'absolute', border: '1px rgba(102, 102, 102, 0.35) solid'}}></div>
+    <div style={{width: 238.01, height: 0, left: 0.50, top: 245.75, position: 'absolute', border: '1px rgba(102, 102, 102, 0.35) solid'}}></div>
+  </div>
+  <div style={{width: 238.51, height: 24.75, left: 726, top: 653, position: 'absolute'}}>
+    <div style={{left: 0, top: 0, position: 'absolute', textAlign: 'center', color: 'rgba(102, 102, 102, 0.35)', fontSize: 20, fontFamily: 'Pretendard', fontWeight: '300', textTransform: 'uppercase', lineHeight: 10, wordWrap: 'break-word'}}>구매일을 입력해주세요</div>
+    <div style={{width: 238.01, height: 0, left: 0.50, top: 24.75, position: 'absolute', border: '1px rgba(102, 102, 102, 0.35) solid'}}></div>
+  </div>
+</div>
   );
 };
 
